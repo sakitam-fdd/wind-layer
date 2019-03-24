a1eb042c36183f5221cdcce2a9b96541ab27c3ef
sakitam-fdd <smilefdd@gmail.com>
2019-03-24 17:19:36 +0800

    feature: docker

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 8080, // 端口
+  port: 3333, // 端口
   staticDir: 'public', // 静态文件目录
   serviceUrl: 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl',
   sourceDataDir: '/sourceData/', // 二进制数据源
