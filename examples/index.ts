7eb99b7e5a17aed24c22eb1d8c27c9a1259e59ca
sakitam-fdd <smilefdd@gmail.com>
2020-01-31 17:22:23 +0800

    update: core Field

--- examples/index.ts
+++ examples/index.ts
@@ -58,15 +58,14 @@ function initMap() {
       const vectorField = new Field({
         xmin: header.lo1, // 一般格点数据是按照矩形范围来切割，所以定义其经纬度范围
         ymin: header.la2,
-        xmax: header.lo2,
+        xmax: 360,
         ymax: header.la1,
         deltaX: 1, // x（经度）增量
         deltaY: 1, // y（维度）增量
-        cols: 359, // 列（可由 `(xmax - xmin) / deltaX` 得到）
-        rows: 180, // 行
+        cols: 360, // 列（可由 `(xmax - xmin) / deltaX` 得到）
+        rows: 181, // 行
         us: uComp.data, // U分量
         vs: vComp.data, // V分量
-        wrapX: true,
       });
 
       console.timeEnd('start');
@@ -74,10 +73,12 @@ function initMap() {
       console.log(res, vectorField);
 
       const windLayer = new WindLayer(vectorField, {
-        colorScale: (m: any) => {
-          console.log(m);
+        colorScale: () => {
+          // console.log(m);
           return '#fff';
         },
+        velocityScale: 1 / 20,
+        paths: 5000,
       });
 
       console.log(map, windLayer);
