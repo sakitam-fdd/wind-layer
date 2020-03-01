de87dc99a079620a0562251dd4620532394dce72
sakitam-fdd <smilefdd@gmail.com>
2020-03-01 21:24:44 +0800

    update: docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,8 +19,16 @@ module.exports = {
     }
   },
   head: [
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'stylesheet', href: '//cdn.jsdelivr.net/npm/openlayers/dist/ol.css' }],
+    ['link', { rel: 'stylesheet', href: '//cache.amap.com/lbs/static/main1119.css' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     // ['link', { rel: 'stylesheet', href: '//at.alicdn.com/t/font_1125947_cnbcy1q7tzm.css' }],
-    // ['script', { src: '//unpkg.com/babel-standalone' }]
+    ['script', { src: '//cdn.jsdelivr.net/npm/proj4@2.6.0/dist/proj4.js' }],
+    ['script', { src: '//cdn.jsdelivr.net/npm/openlayers/dist/ol.js' }],
+    ['script', { src: '//api.map.baidu.com/api?v=3.0&ak=bxFuXXDt1oKdlgu6mXCCnK51cDgDGBLp' }],
   ],
   themeConfig: {
     editLinks: true,
@@ -97,7 +105,19 @@ module.exports = {
               collapsable: false,
               children: [
                 '',
-                'Introduction',
+                'data',
+              ],
+            },
+            {
+              title: '使用示例',
+              collapsable: false,
+              children: [
+                'ol',
+                'ol5',
+                'openlayers',
+                'maptalks',
+                'amap',
+                'bmap',
               ],
             }
           ],
---
+++ docs/zh/start/ol5.md
@@ -0,0 +1,60 @@
+---
+title: openlayers 5
+lang: cn-ZH
+description: 
+order: 1
+---
+
+## 示例
+
+### 基础地图展示
+
+:::demo 基础使用，地图投影默认为 `EPSG:3857` 墨卡托投影。
+
+```demo
+<demo-ol5-wind-base />  
+```
+
+<<<@/examples/components/ol5/base.vue
+
+:::
+
+### 投影变换
+
+此处用于展示各类投影下的风场效果。
+
+#### epsg:4326
+
+:::demo 投影变换，地图投影默认为 `EPSG:4326`, 可以看到地图两极有明显变形;
+
+```demo
+<demo-ol5-wind-epsg4326 />  
+```
+
+<<<@/examples/components/ol5/epsg4326.vue
+
+:::
+
+#### epsg:3413 极投影
+
+:::demo 投影变换，地图投影默认为 `EPSG:3413` 极投影;
+
+```demo
+<demo-ol5-wind-epsg3413 />  
+```
+
+<<<@/examples/components/ol5/epsg3413.vue
+
+:::
+
+#### ESRI:53009 moll
+
+:::demo 投影变换，地图投影默认为 `ESRI:53009`;
+
+```demo
+<demo-ol5-wind-moll />  
+```
+
+<<<@/examples/components/ol5/moll.vue
+
+:::
