4b37a901d5a04bc16b3aa3fcce69169b0fede9f7
sakitam-fdd <smilefdd@gmail.com>
2020-05-24 21:55:08 +0800

    feat: gl core

--- packages/gl-core/__tests__/core.test.ts
+++ packages/gl-core/__tests__/core.test.ts
@@ -1,18 +1,7 @@
-const {
-  isFunction,
-} = require('..');
+describe('gl-core', () => {
+  it('create', () => {
+    const layer = 'layer';
 
-describe('core-utils', () => {
-  it('isFunction', () => {
-      const a = 1;
-      const b = 'string';
-      const c = [1];
-      const d = function() {};
-      const e = function *() {};
-      expect(isFunction(a)).toBe(false);
-      expect(isFunction(b)).toBe(false);
-      expect(isFunction(c)).toBe(false);
-      expect(isFunction(d)).toBe(true);
-      expect(isFunction(e)).toBe(true);
-    });
+    expect(layer).toBeDefined();
+  });
 });
