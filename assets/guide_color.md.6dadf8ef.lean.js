9356b7da79437b79e45c85e550468256e410d6b8
sakitam-fdd <sakitam-fdd@users.noreply.github.com>
2023-06-06 11:45:15 +0000

    deploy: 8026d26523e0693dcdf49e25ebf3a4f9ede6ec88

---
+++ assets/guide_color.md.6dadf8ef.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as o}from"./app.d4ded0dd.js";const i=JSON.parse('{"title":"colorScale","description":"","frontmatter":{},"headers":[],"relativePath":"guide/color.md","lastUpdated":1686051614000}'),l={name:"guide/color.md"},p=o("",6),e=[p];function t(c,r,F,y,D,C){return n(),a("div",null,e)}const _=s(l,[["render",t]]);export{i as __pageData,_ as default};
