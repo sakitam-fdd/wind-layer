c4465101caf96b4e0b5aba9e2c15cba7f3c4b16a
sakitam-fdd <sakitam-fdd@users.noreply.github.com>
2023-07-17 17:51:18 +0000

    deploy: 280e480b6813fbf1e789fdee14e8ab44a655ac19

---
+++ assets/guide_getting-started.md.f1b3f061.lean.js
@@ -0,0 +1,5 @@
+import{_ as a,o as n,c as l,U as s,k as p}from"./chunks/framework.c7fede8e.js";const o="/wind-layer/wind.png",h=JSON.parse('{"title":"快速使用","description":"","frontmatter":{},"headers":[],"relativePath":"guide/getting-started.md","filePath":"guide/getting-started.md","lastUpdated":1689615596000}'),t={name:"guide/getting-started.md"},e=s("",12),r=p("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"sakitam-gis-gl-wind",src:"https://codepen.io/sakitam-fdd/embed/preview/vYjdQbr?default-tab=result",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
+  See the Pen <a href="https://codepen.io/sakitam-fdd/pen/vYjdQbr">
+  sakitam-gis-gl-wind</a> by FDD (<a href="https://codepen.io/sakitam-fdd">@sakitam-fdd</a>)
+  on <a href="https://codepen.io">CodePen</a>.
+`,-1),c=s("",24),F=[e,r,c];function D(y,i,d,C,m,g){return n(),l("div",null,F)}const w=a(t,[["render",D]]);export{h as __pageData,w as default};
