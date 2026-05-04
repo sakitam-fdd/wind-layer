84de2ea4889498f76b43491b3dd08e1ae63f43f7
jcarbou <jcarbou@groupcls.com>
2026-05-04 10:18:29 +0200

    Update doc

--- packages/gl-core/src/source/urlData.ts
+++ packages/gl-core/src/source/urlData.ts
@@ -1,9 +1,9 @@
 /**
  * Generate WMS GepMap BBOX parameter
  * @param data - tile url data
- * @returns 
+ * @returns
  */
-export function bboxEpsg3857({ x, y, z } : {x:number, y:number, z:number, [key: string]: any}) {
+export function bboxEpsg3857({ x, y, z }: { x: number; y: number; z: number; [key: string]: any }) {
   const gridSize = Math.pow(2, z); // Total world grid at zoom level
   const mercatorSize = 2 * 20037508.34; // Half the world's circumference in EPSG:3857 in meters
   const cellSize = mercatorSize / gridSize; // TMS cell size in meters
@@ -14,14 +14,12 @@ export function bboxEpsg3857({ x, y, z } : {x:number, y:number, z:number, [key: 
   const maxY = mercatorSize / 2 - (y / gridSize) * mercatorSize;
   const minY = maxY - cellSize;
 
-  return [
-    minX.toFixed(2),
-    minY.toFixed(2),
-    maxX.toFixed(2),
-    maxY.toFixed(2),
-  ].join(",");
+  return [minX.toFixed(2), minY.toFixed(2), maxX.toFixed(2), maxY.toFixed(2)].join(',');
 }
 
+/**
+ * Url Data available for all requests
+ */
 export const DEFAULT_URL_DATA = {
-    'bbox-epsg-3857': bboxEpsg3857
-}
+  'bbox-epsg-3857': bboxEpsg3857,
+};
